83f19ee05e45e7399725178b289b6e70dc3018ea
dcastineyra <30376556+dcastineyra@users.noreply.github.com>
2017-11-22 18:20:22 -0600

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,13 @@
+## Reading the files
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+## Making a data frame with the total emissions per year
+tot_emi <- data.frame(Year = unique(NEI$year),Total_Emissions = tapply(NEI$Emissions,NEI$year,sum))
+## converting total emissions into thousands for easier reading
+tot_emi$Total_Emissions<-tot_emi$Total_Emissions/1000
+## plpotting
+png("plot1.png",width = 480,height = 480)
+plot(tot_emi,xlab = "Year",ylab = "Total Emissions (Thousands)",main = "Total PM2.5 Emissions per Year",xaxt='n')
+axis(1,tot_emi$Year)
+lines(tot_emi)
+dev.off()
